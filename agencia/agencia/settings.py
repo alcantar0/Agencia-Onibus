7e10e7411a8571a8434d9ca8448b9524367eb873
Pedro Henrique de Sousa Alcântara <alcantarapedro69@yahoo.com>
2022-12-13 17:06:35 -0300

    Pasta para static criados.

--- agencia/agencia/settings.py
+++ agencia/agencia/settings.py
@@ -109,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
